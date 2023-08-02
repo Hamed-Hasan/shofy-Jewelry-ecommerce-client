4028958751641a9752e5fcf6a2a874c49e764f73
John Doe <hamedhasan422421@gmail.com>
2023-08-02 20:50:42 +0300

    🔧 refactor: Update MobileMenus component & optimize imports.
    🔄 Replace deprecated Image component with the latest Next.js Image.
    🚀 Optimize the code and remove unnecessary variables and imports.
    
    🎨 Refactor the rendering logic for better readability and maintainability.

--- src/components/common/mobile-menus.jsx
+++ src/components/common/mobile-menus.jsx
@@ -6,7 +6,19 @@ import ProductItem from "../products/electronics/product-item";
 import ErrorMsg from "./error-msg";
 import { HomeNewArrivalPrdLoader } from "../loader";
 import { useGetProductTypeQuery } from "@/redux/features/productApi";
+// internal
+import insta_1 from '@assets/img/instagram/4/instagram-1.jpg';
+import insta_3 from '@assets/img/instagram/4/instagram-3.jpg';
+import insta_4 from '@assets/img/instagram/4/instagram-4.jpg';
+import insta_6 from '@assets/img/instagram/4/instagram-6.jpg';
 
+// instagram data 
+const instagram_data = [
+  { id: 1, link: 'https://www.instagram.com/', img: insta_1 },
+  { id: 2, link: 'https://www.instagram.com/', img: insta_3 },
+  { id: 3, link: 'https://www.instagram.com/', img: insta_4 },
+  { id: 4, link: 'https://www.instagram.com/', img: insta_6 },
+]
 const MobileMenus = () => {
   const [isActiveMenu,setIsActiveMenu] = useState("")
 
@@ -73,9 +85,20 @@ const MobileMenus = () => {
                 </a>
                 <div className={`home-menu tp-submenu tp-mega-menu ${isActiveMenu === menu.title ? 'active':''}`}>
                   <div className="row row-cols-1 row-cols-lg-4 row-cols-xl-5">
-                  <div className="tp-product-arrival-slider fix">
-    {content}
-  </div>
+                  <div className="row row-cols-lg-4 row-cols-sm-2 row-cols-1 gx-2 gy-2 gy-lg-0">
+            {instagram_data.map((item, i) => (
+              <div className="col" key={i}>
+                <div className="tp-instagram-item-2 w-img">
+                  <Image src={item.img} alt="instagram img" style={{ width: '100%', height: '100%' }} />
+                  <div className="tp-instagram-icon-2">
+                    <a href={item.link} target="_blank" className="popup-image">
+                      <i className="fa-brands fa-instagram"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
                     {menu.home_pages.map((home, i) => (
                       <div key={i} className="col">
                         <div className="home-menu-item">
